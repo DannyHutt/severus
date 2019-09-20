675ff396a74e17fe9ede3f351d91c7893b07593e
Dan Gauthier <dan.gauthier@cantina.co>
2019-09-19 21:57:58 -0400

    functionality

---
+++ js/scripts.js
@@ -0,0 +1,273 @@
+data = {surveys : {
+          beliefs: {
+            questions: [
+              {question: "I need 8 hours of sleep to feel refreshed and function well during the day."},
+              {question: "When I do not get proper amount of sleep on a given night, I need to catch up on the next day by napping or on the next night by sleeping longer."},
+              {question: "I am concerned that chronic insomnia may have serious consequences for my physical health."},
+              {question: "I am worried that I may lose control over my abilities to sleep."},
+              {question: "After a poor night’s sleep, I know that it will interfere with my daily activities on the next day."},
+              {question: "In order to be alert and function well during the day, I am better off taking a sleeping pill rather than having a poor night’s sleep."},
+              {question: "When I feel irritable, depressed, or anxious during the day, it is mostly because I did not sleep well the night before."},
+              {question: "When I sleep poorly on one night, I know that it will disturb my sleep schedule for the whole week."},
+              {question: "Without an adequate night’s sleep, I can hardly function the next day."},
+              {question: "I can’t ever predict whether I will have a good or poor night’s sleep."},
+              {question: "I have little ability to manage the negative consequences of disturbed sleep."},
+              {question: "When I feel tired, have no energy, or just seem not to function well during the day, it is generally because I did not sleep well the night before."},
+              {question: "I believe that insomnia is essentially a result of a chemical imbalance."},
+              {question: "I feel that insomnia is ruining my ability to enjoy life and prevents me from doing what I want."},
+              {question: "Medication is probably the only solution to sleeplessness."},
+              {question: "I avoid or cancel obligations (social, family, occupational) after a poor night’s sleep."}
+            ],
+            headline:"Several statements reflecting people’s beliefs and attitudes about sleep are listed below. Please indicate (by circling the number) to what extent you personally agree or disagree with each statement. There is no right or wrong answer. For each statement, circle a number that best reflects your personal experience. Consider the whole scale, rather than only the extremes of the continuum.",
+          },
+          depression: {
+            questions: [
+              {
+                question: "Difficulty falling asleep",
+                headline:"Please rate the CURRENT (i.e last two weeks) SEVERITY of your insomnia problem(s)",
+                answers: [
+                  {answer:"None"},
+                  {answer:"Mild"},
+                  {answer:"Moderate"},
+                  {answer:"Severe"},
+                  {answer:"Very Severe"}
+                ]
+              },
+              {
+                question: "Difficulty staying asleep",
+                headline:"Please rate the CURRENT (i.e last two weeks) SEVERITY of your insomnia problem(s)",
+                answers: [
+                  {answer:"None"},
+                  {answer:"Mild"},
+                  {answer:"Moderate"},
+                  {answer:"Severe"},
+                  {answer:"Very Severe"}
+                ]
+              },
+              {
+                question: "Problems waking up too early",
+                headline:"Please rate the CURRENT (i.e last two weeks) SEVERITY of your insomnia problem(s)",
+                answers: [
+                  {answer:"None"},
+                  {answer:"Mild"},
+                  {answer:"Moderate"},
+                  {answer:"Severe"},
+                  {answer:"Very Severe"}
+                ]
+              },
+              {
+                question: "How SATISFIED/DISSATISFIED are you with your CURRENT sleep pattern?",
+                headline:"Please rate the CURRENT (i.e last two weeks) SEVERITY of your insomnia problem(s)",
+                answers: [
+                  {answer:"Very satisfied"},
+                  {answer:"Satisfied"},
+                  {answer:"Moderately satisfied"},
+                  {answer:"Dissatisfied"},
+                  {answer:"Very dissatisfied"}
+                ]
+              },
+              {
+                question: "How NOTICEABLE to others do you think your sleep problem is in terms of impairing the quality of your life?",
+                headline:"Please rate the CURRENT (i.e last two weeks) SEVERITY of your insomnia problem(s)",
+                answers: [
+                  {answer:"Not at all noticeable"},
+                  {answer:"A little"},
+                  {answer:"Somewhat"},
+                  {answer:"Much"},
+                  {answer:"Very much noticeable"}
+                ]
+              },
+              {
+                question: "How WORRIED/DISTRESSED are you about your current sleep problem?",
+                headline:"Please rate the CURRENT (i.e last two weeks) SEVERITY of your insomnia problem(s)",
+                answers: [
+                  {answer:"Not at all worried"},
+                  {answer:"A little"},
+                  {answer:"Somewhat"},
+                  {answer:"Much"},
+                  {answer:"Very much worried"}
+                ]
+              },
+              {
+                question: "To what extent do you consider your sleep problem to INTERFERE with your daily functioning (e.g. daytime fatigue, mood, ability to function at work/daily chores, concentration, memory, mood, etc.) CURRENTLY?",
+                headline:"Please rate the CURRENT (i.e last two weeks) SEVERITY of your insomnia problem(s)",
+                answers: [
+                  {answer:"Not at all interfering"},
+                  {answer:"A little"},
+                  {answer:"Somewhat"},
+                  {answer:"Much"},
+                  {answer:"Very much interfering"}
+                ]
+              }
+            ]
+          },
+          anxiety: {
+            questions: [
+              {
+                question: "Feeling nervous, anxious, or on edge",
+                headline:"Over the last two weeks, how often have you been bothered by the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Not being able to stop or control worrying",
+                headline:"Over the last two weeks, how often have you been bothered by the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Worrying too much about different things",
+                headline:"Over the last two weeks, how often have you been bothered by the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Trouble relaxing",
+                headline:"Over the last two weeks, how often have you been bothered by the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Being so restless that it is hard to sit still",
+                headline:"Over the last two weeks, how often have you been bothered by the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Becoming easily annoyed or irritable",
+                headline:"Over the last two weeks, how often have you been bothered by the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Feeling afraid, as if something awful might happen",
+                headline:"Over the last two weeks, how often have you been bothered by the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              }
+            ]
+          },
+          insomnia: {
+            questions: [
+              {
+                question: "Little interest or pleasure in doing things",
+                headline:"Over the last two weeks, how often have you been bothered by any of the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Feeling down depressed or hopeless",
+                headline:"Over the last two weeks, how often have you been bothered by any of the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Trouble falling or staying asleep, or sleeping too much",
+                headline:"Over the last two weeks, how often have you been bothered by any of the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Feeling tired or having little energy",
+                headline:"Over the last two weeks, how often have you been bothered by any of the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Poor appetite or overeating",
+                headline:"Over the last two weeks, how often have you been bothered by any of the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Feeling bad about yourself - or that you are a failure or have let yourself or your family down",
+                headline:"Over the last two weeks, how often have you been bothered by any of the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Trouble concentrating on things, such as reading the newspaper or watching television",
+                headline:"Over the last two weeks, how often have you been bothered by any of the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Moving or speaking so slowly that other people could have noticed. Or the opposite - being so figety or restless that you have been moving around a lot more than usual",
+                headline:"Over the last two weeks, how often have you been bothered by any of the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              },
+              {
+                question: "Thoughts that you would be better off dead, or of hurting yourself",
+                headline:"Over the last two weeks, how often have you been bothered by any of the following problems?",
+                answers: [
+                  {answer:"Not at all"},
+                  {answer:"Several days"},
+                  {answer:"More than half the days"},
+                  {answer:"Nearly every day"}
+                ]
+              }
+            ]
+          }
+        }
+      };
